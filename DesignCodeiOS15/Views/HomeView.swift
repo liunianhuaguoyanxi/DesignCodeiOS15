de0ad4d681a33be12a0aa32f0af8b7d7fd13d817
cdsbZhangWei <zhangwei@cdsb.com>
2022-03-16 17:51:37 +0800

    ...

--- DesignCodeiOS15/Views/HomeView.swift
+++ DesignCodeiOS15/Views/HomeView.swift
@@ -16,7 +16,7 @@ struct HomeView: View {
     @State var showCourse = false
     @State var selectedIndex = 0;
     @EnvironmentObject var model:Model
-    @AppStorage("isListMode") var isLiteMode = true
+    @AppStorage("isLiteMode") var isLiteMode = true
     
     var body: some View {
         ZStack {
@@ -111,7 +111,7 @@ struct HomeView: View {
                         .frame(maxWidth: .infinity)
                         .padding(.vertical, 40)
                         .rotation3DEffect(.degrees(minX / (-10)), axis: (x: 0, y: 1, z: 0)  )
-                        .shadow(color: Color("Shadow").opacity(isLiteMode ? 0 : 0.9), radius: 5, x: 0, y: 3)
+                        .shadow(color: Color("Shadow").opacity(isLiteMode ? 0 : 0.3), radius: 5, x: 0, y: 3)
                         .blur(radius: abs(minX / 40))
                         .overlay(   Image(course.image)
                                     .resizable()
