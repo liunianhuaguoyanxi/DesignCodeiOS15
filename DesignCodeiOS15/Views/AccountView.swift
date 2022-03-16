873ba2b7595609710c351924205b73ec5a4f7639
cdsbZhangWei <zhangwei@cdsb.com>
2022-03-16 17:48:15 +0800

    ...

--- DesignCodeiOS15/Views/AccountView.swift
+++ DesignCodeiOS15/Views/AccountView.swift
@@ -13,6 +13,7 @@ struct AccountView: View {
     @State var address: Address = Address(id: 1, country: "Cannada")
     @Environment(\.dismiss) var dismiss
     @AppStorage("isLogged") var isLogged = false
+    @AppStorage("isLiteMode") var isLiteMode = true
     @ObservedObject var coinModel = CoinModel()
     
     func fetchAddress() async {
@@ -35,6 +36,13 @@ struct AccountView: View {
                 profile
                 
                 menu
+                
+                Section{
+                    Toggle(isOn: $isLiteMode){
+                        Label("List Mode", systemImage: isLiteMode ? "tortoise" : "hare")
+                    }
+                }
+                .accentColor(.primary)
                 
                 links
                 
--- DesignCodeiOS15/Views/HomeView.swift
+++ DesignCodeiOS15/Views/HomeView.swift
@@ -16,6 +16,7 @@ struct HomeView: View {
     @State var showCourse = false
     @State var selectedIndex = 0;
     @EnvironmentObject var model:Model
+    @AppStorage("isListMode") var isLiteMode = true
     
     var body: some View {
         ZStack {
@@ -110,7 +111,7 @@ struct HomeView: View {
                         .frame(maxWidth: .infinity)
                         .padding(.vertical, 40)
                         .rotation3DEffect(.degrees(minX / (-10)), axis: (x: 0, y: 1, z: 0)  )
-                        .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
+                        .shadow(color: Color("Shadow").opacity(isLiteMode ? 0 : 0.9), radius: 5, x: 0, y: 3)
                         .blur(radius: abs(minX / 40))
                         .overlay(   Image(course.image)
                                     .resizable()
