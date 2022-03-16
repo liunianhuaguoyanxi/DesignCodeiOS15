f8f1185bfc659e2b855832e020e691ab6c346703
cdsbZhangWei <zhangwei@cdsb.com>
2022-03-16 17:34:09 +0800

    ...

--- DesignCodeiOS15/Views/AccountView.swift
+++ DesignCodeiOS15/Views/AccountView.swift
@@ -13,6 +13,7 @@ struct AccountView: View {
     @State var address: Address = Address(id: 1, country: "Cannada")
     @Environment(\.dismiss) var dismiss
     @AppStorage("isLogged") var isLogged = false
+    @ObservedObject var coinModel = CoinModel()
     
     func fetchAddress() async {
         do{
@@ -36,6 +37,8 @@ struct AccountView: View {
                 menu
                 
                 links
+                
+                coins
 
                 Button{
                     isLogged = false
@@ -47,9 +50,11 @@ struct AccountView: View {
             }
             .task {
                 await fetchAddress()
+                await coinModel.fetchCoins()
             }
             .refreshable {
                 await fetchAddress()
+                await coinModel.fetchCoins()
             }
             .listStyle(.insetGrouped)
             .navigationTitle("Account")
@@ -157,6 +162,31 @@ struct AccountView: View {
         }
         .accentColor(.primary)
         .listRowSeparator(.hidden)
+    }
+    
+    var coins: some View {
+        Section(header: Text("Coins")){
+            ForEach(coinModel.coins){coin in
+                HStack {
+                    AsyncImage(url: URL(string: coin.logo)){image in
+                        image.resizable()
+                            .aspectRatio(contentMode: .fit)
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    .frame(width: 32, height: 32)
+                    
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text(coin.coin_name)
+                        Text(coin.acronym)
+                            .font(.caption)
+                            .foregroundColor(.secondary)
+                    }
+                }
+
+                
+            }
+        }
     }
     
     var pinButton: some View
